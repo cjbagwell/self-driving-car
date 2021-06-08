05df8622619947126b861490dd362b41baf01c9a
Jordan <cjbagwell@crimson.ua.edu>
2021-06-08 15:30:04 -0500

    Fixed include errors (set cxx standard) and gtest now working

---
+++ src/localization/test/LocalizationTests.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<ostream> 
+#include<vector>
+#include<gtest/gtest.h>
+#include "../rotations.h"
+
+
+using namespace std;
+
+TEST(LocalizationTests, MacrosTest){
+    EXPECT_TRUE(true);
+}
