6ab45b93c3fe577111b93ee013cbb98e32f1523e
Jordan <cjbagwell@crimson.ua.edu>
2021-06-13 17:35:12 -0500

    Added opencv as submodule

---
+++ src/localization/VisualOdometer.h
@@ -0,0 +1,24 @@
+/**
+ * @file VisualOdometer.h
+ * @author C. Jordan Bagwell (cjbagwell@crimson.ua.edu)
+ * @brief 
+ * @TODO: some stuff here
+ * @version 0.1
+ * @date 2021-06-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef VISUAL_ODOMETER_H
+#define VISUAL_ODOMETER_H
+
+
+#include<iostream>
+#include<ostream>
+#include<vector>
+#include<opencv2/highgui.hpp>
+
+
+
+#endif
